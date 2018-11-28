cdd8703995fdcfb3f45898d2106954bf35851ace
rm792745 <43928856+rm792745@users.noreply.github.com>
2018-11-28 11:41:36 +0100

    Add files via upload

---
+++ unitlieu.pas
@@ -0,0 +1,73 @@
+unit UnitLieu;
+
+
+
+{$mode objfpc}{$H+}
+
+
+
+interface
+
+type
+
+  lieu=record
+
+    nom : string;
+    ressource : string;
+    acces : Boolean;
+
+  end;
+
+var
+  fermeAc:boolean;
+
+function foret():lieu;
+function lac():lieu;
+function ferme():lieu;
+procedure accesF();
+procedure initialisationLieu();
+
+implementation
+
+uses UnitRessource;
+
+procedure initialisationLieu();
+begin
+   fermeAc := FALSE
+end;
+
+function lac():lieu;
+
+begin
+  lac.nom:='lac';
+  lac.ressource:='Poisson';
+  lac.acces := TRUE;
+end;
+
+function foret():lieu;
+
+begin
+  foret.nom:='la fôret';
+  foret.ressource:='Bois';
+  lac.acces := TRUE;
+end;
+
+function ferme():lieu;
+begin
+  ferme.nom:='la ferme';
+  ferme.ressource:='Legume';
+  ferme.acces := fermeAc;
+end;
+
+procedure accesF();
+begin
+  if nbBois >=50 then
+  begin
+     writeln('la ferme est consstruite');
+     fermeAc:=TRUE;
+     rmBois(50);
+  end
+  else writeln('vous n''avez pas assez de bois');
+end;
+
+end.
